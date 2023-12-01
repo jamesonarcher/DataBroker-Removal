90dbb60643de67dcfd245d3fb80cb781e51c9112
jamesonarcher <jamesonmarcher@gmail.com>
2023-12-01 16:28:41 -0500

    Initial commit

---
+++ src/components/Logo.tsx
@@ -0,0 +1,9 @@
+export function Logo() {
+  return (
+    <a href="/">
+      <div class="flex items-center">
+        <img src="/logo.png" alt="Visible Labs" width={200} height={75} />
+      </div>
+    </a>
+  )
+}
